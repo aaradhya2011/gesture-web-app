b65f0561f7170c4cb99653257bdaaeb99ab542ef
aaradhya2011 <80405586+aaradhya2011@users.noreply.github.com>
2022-01-15 13:41:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -17,9 +17,39 @@ function take_snapshot()
 
 console.log('ml5 version', ml5.version);
 
-var classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/zqfXn51oK/',modelLoaded);
+var classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/zqfXn51oK/model.json',modelLoaded);
 
 function modelLoaded()
     {
     console.log("modelLoaded")
+    }
+
+    function check()
+    {
+        img = document.getElementById('captured_image');
+        classifier.classify(img,gotResult);
+    }
+
+    function gotResult(error,results) {
+        if(error) {
+            console.error(error);
+        }else {
+            console.log (results);
+            document.getElementById("result_emotion_name").innerHTML = results[0].label;
+            prediction_1 = [0].label;
+            speak();
+            if(results[0].label == "amazing")
+            {
+                document.getElementById("update_emoji").innerHTML = "&#128076";
+            }
+            if(results[0].label == "best")
+            {
+            document.getElementById("update_emoji").innerHTML = "&#128077";
+            }
+                if(results[0].label =="victory")
+            {
+                document.getElementById("update_emoji").innerHTML = "&#9996";
+            }  
+        }
+
     }
